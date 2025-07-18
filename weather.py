64646e90da9857bb085f94b34576de662cab0ff0
Emily Sheridan <emily.sheridan@rwwa.com.au>
2025-07-18 13:15:10 +0800

    initial commit

--- weather.py
+++ weather.py
@@ -24,7 +24,11 @@ def convert_date(iso_string):
     Returns:
         A date formatted like: Weekday Date Month Year e.g. Tuesday 06 July 2021
     """
-    pass
+    date = datetime.fromisoformat(iso_string)
+    return date.strftime("%A %d %B %Y")
+    
+
+
 
 
 def convert_f_to_c(temp_in_fahrenheit):
@@ -35,7 +39,9 @@ def convert_f_to_c(temp_in_fahrenheit):
     Returns:
         A float representing a temperature in degrees Celcius, rounded to 1 decimal place.
     """
-    pass
+    tempf = float(temp_in_fahrenheit)
+    celsius = (tempf-32) * 5/9
+    return round(celsius,1)
 
 
 def calculate_mean(weather_data):
@@ -46,7 +52,11 @@ def calculate_mean(weather_data):
     Returns:
         A float representing the mean value.
     """
-    pass
+    temp_values=[]
+    for item in weather_data:
+        temp_values.append(float(item))
+    mean_value = sum(temp_values) / len(temp_values)
+    return mean_value
 
 
 def load_data_from_csv(csv_file):
@@ -57,7 +67,17 @@ def load_data_from_csv(csv_file):
     Returns:
         A list of lists, where each sublist is a (non-empty) line in the csv file.
     """
-    pass
+
+    import csv
+
+    data=[]
+    with open(csv_file, "r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            print(', '.join(row))
+
+    return data
+
 
 
 def find_min(weather_data):
@@ -68,7 +88,15 @@ def find_min(weather_data):
     Returns:
         The minimum value and it's position in the list. (In case of multiple matches, return the index of the *last* example in the list.)
     """
-    pass
+    if not weather_data:
+        return ()
+    location_index=0
+    minimum_value = float(weather_data[0])
+    for index, value in enumerate(weather_data):
+        if float(value)<=minimum_value:
+            location_index=index
+            minimum_value=float(value)
+    return (float(minimum_value), location_index)
 
 
 def find_max(weather_data):
@@ -79,7 +107,16 @@ def find_max(weather_data):
     Returns:
         The maximum value and it's position in the list. (In case of multiple matches, return the index of the *last* example in the list.)
     """
-    pass
+    
+    if not weather_data:
+        return ()
+    location_index=0
+    maximum_value = float(weather_data[0])
+    for index, value in enumerate(weather_data):
+        if float(value)>=maximum_value:
+            location_index=index
+            maximum_value=float(value)
+    return (float(maximum_value), location_index)
 
 
 def generate_summary(weather_data):
